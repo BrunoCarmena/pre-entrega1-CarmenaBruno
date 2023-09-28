96e384b5910f50676a99fe4bc6887c2ec1bb74a8
bruno <bruno.s.carmena@hotmail.com>
2023-09-28 03:27:09 -0300

    se agrego while

--- js/script.js
+++ js/script.js
@@ -1,84 +1,50 @@
-function verificarEdad() {
-    var edad = parseInt(prompt("Por favor, ingresa tu edad:"));
+function seleccionarVino() {
+    var tipoVino = prompt("¿Qué tipo de vino prefieres? (tinto, blanco, rosado)").toLowerCase();
 
-    if (!isNaN(edad)) {
-        if (edad >= 18) {
-            var tieneCarnet = prompt("¿Tienes carnet de conducir? (responde sí o no)").toLowerCase();
-            if (tieneCarnet === "si") {
-                alert("¡Puedes conducir!");
-            } else if (tieneCarnet === "no") {
-                alert("Lo siento, no puedes conducir sin carnet.");
-            } else {
-                alert("Respuesta inválida. Por favor, responde sí o no.");
-            }
-        } else {
-            alert("Eres menor de edad, no puedes conducir.");
-        }
-
-        // Determinar la etapa de la vida
-        if (edad >= 65) {
-            alert("Estás en la vejez.");
-        } else if (edad >= 35) {
-            alert("Estás en la adultez plena.");
-        } else if (edad >= 18) {
-            alert("Estás en la juventud.");
-        } else if (edad >= 13) {
-            alert("Estás en la adolescencia.");
-        } else {
-            alert("Eres un niño.");
-        }
-    } else {
-        alert("Por favor, ingresa un número válido.");
-    }
-}
-
-// Llamar a la función
-verificarEdad();
-
-
-
-//SWTICH
-
-function obtenerContinente(pais) {
-    var continente;
-
-    pais = pais.toLowerCase();
-
-    switch (pais) {
-        case "españa":
-        case "francia":
-        case "alemania":
-        case "italia":
-        case "inglaterra":
-            continente = "Europa";
-            break;
-        case "estados unidos":
-        case "canadá":
-        case "méxico":
-        case "brasil":
-        case "argentina":
-            continente = "América";
-            break;
-        case "china":
-        case "japón":
-        case "corea del sur":
-        case "corea del norte":
-        case "india":
-            continente = "Asia";
-            break;
+    switch (tipoVino) {
+        case "tinto":
+            return 20;
+        case "blanco":
+            return 18;
+        case "rosado":
+            return 15;
         default:
-            continente = "No identificado";
+            return 0;
+    }
+}
+
+function calcularDescuento(total) {
+    var descuento = 0;
+
+    if (total > 50) {
+        descuento = total * 0.1; // 10% de descuento si el total es mayor a 50
     }
 
-    return continente;
+    return descuento;
 }
 
-var pais = prompt("Por favor, ingresa un país:");
-var continente = obtenerContinente(pais);
+function realizarCompra() {
+    var total = 0;
+    var continuar = true;
 
-if (continente !== "No identificado") {
-    alert("El país " + pais + " está en el continente de " + continente);
-} else {
-    alert("No pudimos identificar el continente para el país ingresado.");
+    while (continuar) {
+        var precio = seleccionarVino();
+
+        if (precio === 0) {
+            alert("Tipo de vino no válido. Por favor, elige un tipo de vino válido.");
+        } else {
+            total += precio;
+
+            continuar = confirm("¿Deseas agregar más vinos a tu compra?");
+        }
+    }
+
+    var descuento = calcularDescuento(total);
+    var totalConDescuento = total - descuento;
+
+    alert("Total sin descuento: $" + total.toFixed(2) + "\nDescuento: $" + descuento.toFixed(2) + "\nTotal con descuento: $" + totalConDescuento.toFixed(2));
 }
+
+realizarCompra();
+
 
