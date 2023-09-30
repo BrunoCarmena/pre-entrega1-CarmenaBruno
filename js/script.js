a75e93ef0f699a1c25d250b432395b5bf0d28442
bruno <bruno.s.carmena@hotmail.com>
2023-09-30 04:12:15 -0300

    modificaciones en function y switch

--- js/script.js
+++ js/script.js
@@ -1,46 +1,66 @@
-let saldo = 100000;
-let propiedad = null;
+function gestionarPropiedad() {
+    let saldo = parseFloat(prompt("Ingrese su saldo inicial (entre 50 y 100,000,000):"));
 
-while (true) {
-    let opcion = prompt(`Tu saldo actual es $${saldo}. ¿Qué deseas hacer? (comprar, vender, alquilar, salir)`);
+    if (isNaN(saldo) || saldo < 50 || saldo > 100000000) {
+        alert("Por favor, ingrese un saldo válido entre 50 y 100,000,000.");
+        return;
+    }
 
-    switch (opcion) {
-        case 'comprar':
-            if (saldo >= 50000) {
-                propiedad = 'comprada';
-                saldo -= 50000;
-                alert('Has comprado la propiedad. Tu saldo actual es: $' + saldo);
-            } else {
-                alert('No tienes suficiente dinero para comprar la propiedad.');
-            }
-            break;
+    let propiedad = null;
 
-        case 'vender':
-            if (propiedad === 'comprada') {
-                propiedad = null;
-                saldo += 60000;
-                alert('Has vendido la propiedad. Tu saldo actual es: $' + saldo);
-            } else {
-                alert('No tienes una propiedad para vender.');
-            }
-            break;
+    while (true) {
+        let opcion = prompt(`Tu saldo actual es $${saldo}. ¿Qué deseas hacer? (comprar, vender, alquilar, salir)`);
 
-        case 'alquilar':
-            if (propiedad === 'comprada') {
-                saldo += 3000;
-                alert('Has alquilado la propiedad. Tu saldo actual es: $' + saldo);
-            } else {
-                alert('No tienes una propiedad para alquilar.');
-            }
-            break;
+        if (!opcion) {
+            alert('Por favor, ingresa una opción.');
+            continue;
+        }
 
-        case 'salir':
-            alert('Gracias por usar nuestro servicio. Tu saldo final es: $' + saldo);
-            return;
+        opcion = opcion.toLowerCase();
 
-        default:
-            alert('Opción no válida. Por favor, elige una opción válida.');
+        switch (opcion) {
+            case 'comprar':
+                let precioInmueble = 50000;
+                let descuento = precioInmueble * 0.15;
+
+                if (saldo >= (precioInmueble - descuento)) {
+                    propiedad = 'comprada';
+                    saldo -= (precioInmueble - descuento);
+                    alert(`Has comprado la propiedad con un 15% de descuento. Tu saldo actual es: $${saldo}`);
+                } else {
+                    alert('No tienes suficiente dinero para comprar la propiedad.');
+                }
+                break;
+
+            case 'vender':
+                if (propiedad === 'comprada') {
+                    propiedad = null;
+                    saldo += 60000;
+                    alert('Has vendido la propiedad. Tu saldo actual es: $' + saldo);
+                } else {
+                    alert('No tienes una propiedad para vender.');
+                }
+                break;
+
+            case 'alquilar':
+                if (propiedad === 'comprada') {
+                    saldo += 3000;
+                    alert('Has alquilado la propiedad. Tu saldo actual es: $' + saldo);
+                } else {
+                    alert('No tienes una propiedad para alquilar.');
+                }
+                break;
+
+            case 'salir':
+                alert('Gracias por usar nuestro servicio. Tu saldo final es: $' + saldo);
+                return;
+
+            default:
+                alert('Opción no válida. Por favor, elige una opción válida.');
+        }
     }
 }
+
+gestionarPropiedad();
 
 
